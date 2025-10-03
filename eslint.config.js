270a4f97edb86056cb564f6110a78d1a78eea4fe
Thiago de Castro <thiago.castro@daimlertruck.com>
2025-10-03 22:15:57 +0100

    chore: updated eslint ecma version

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ export default [
   {
     files: ['**/*.js', '**/*.ts'],
     languageOptions: {
-      ecmaVersion: 2021,
+      ecmaVersion: 2022,
       sourceType: 'module',
     },
     plugins: {
