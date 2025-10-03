9f0fa456328ba482f1980610599f6a268c032ab7
Thiago de Castro <thiago.castro@daimlertruck.com>
2025-10-03 21:15:01 +0100

    chore: Added dot-env structure

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig, devices } from '@playwright/test';
 import { Time } from "./src/helpers/consts";
+import * as dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const envTarget = process.env.ENV_TARGET || 'dev';
+dotenv.config({ path: path.resolve(__dirname, `.env.${envTarget}`) });
 
 export default defineConfig({
   testDir: './src/tests',
@@ -14,6 +21,7 @@ export default defineConfig({
   reporter: "html",
 
   use: {
+    baseURL: process.env.BASE_URL,
     headless: true
   },
 
